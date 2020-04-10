fcd3a3dbd178864ddbd722f86f71a9301361d92b
TJ VanToll <tj.vantoll@gmail.com>
2020-04-10 14:54:27 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import Gong from './components/Gong';
+import Counter from "./components/Counter";
 
 function App() {
+  const [isItRaveTime, setIsRaveTime] = React.useState(false);
+  const onGongStart = () => {
+    setIsRaveTime(true);
+  }
+  const onGongEnd = () => {
+    setIsRaveTime(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class={isItRaveTime ? "bg-spring bg-rave" : "bg-spring"}>
+      <h1>Mr. Milks just closed a deal!</h1>
+      <Gong onGongStart={onGongStart} onGongEnd={onGongEnd} />
+      <Counter />
     </div>
   );
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Counter() {
+  const [count, setCount] = React.useState(0);
+  const [displayCount, setDisplayCount] = React.useState("00000000");
+
+  React.useEffect(() => {
+    setInterval(() => {
+      setCount((count) => {
+        let countIWant = count + 1;
+        setCount(countIWant);
+        setDisplayCount(countIWant.toString().padStart(8, "0"));
+      });
+    }, 1);
+
+  }, []);
+
+  return (
+    <div class="counter">
+      {displayCount}
+    </div>
+  )
+}
